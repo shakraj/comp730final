5301b6e865d04889516db3372e8749652404e6fa
shak rajbhandari <shak@shaks-MacBook-Pro-2.local>
2020-04-13 03:20:58 -0400

    new commit

---
+++ BakedGoods.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comp730finals;
+
+/**
+ *
+ * @author shak
+ */
+public interface BakedGoods {
+Public int getPrice();
+
+Public String getDescription();
+
+Public String getSellByDate();
+    
+}
