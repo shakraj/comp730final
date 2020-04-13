6067ccabe5e46e018e7f8193221ee64ccc1c8653
shak rajbhandari <shak@shaks-MacBook-Pro-2.local>
2020-04-13 03:50:53 -0400

    new commit

---
+++ OtherSciFiSpaceShips.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comp730finals;
+
+/**
+ *
+ * @author shak
+ */
+public class OtherSciFiSpaceShips extends SpaceShip {
+    public void getTonnage(){
+    
+
+        System.out.println("1000000")
+}
+ public static void main(String args[]){
+	SpaceShip obj = new OtherSciFiSpaceShips();
+	obj.getTonnage();
+    
+
+        }
+ public void getName(){
+     System.out.println("TARDIS")
+ }
+  public static void main(String args[]){
+	SpaceShip obj = new OtherSciFiSpaceShips();
+	obj.getName();
+ 
+}
+  public void getFranchise(){
+      System.out.println("Doctor Who")
+  }
+   public static void main(String args[]){
+	SpaceShip obj = new OtherSciFiSpaceShips();
+	obj.getFranchise();
+  
+}
+}
+    
+}
