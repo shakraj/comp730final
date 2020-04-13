ae5dd951ef030c920614d0380d7b022348ada2f4
shak rajbhandari <shak@shaks-MacBook-Pro-2.local>
2020-04-13 03:13:34 -0400

    First commit

---
+++ Cookie.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comp730finals;
+
+/**
+ *
+ * @author shak
+ */
+public class Cookie implements BakedGoods  {
+    public void getPrice(){
+    
+
+        System.out.println("2")
+}
+ public static void main(String args[]){
+	BakedGoods obj = new Cookie();
+	obj.getPrice();
+    
+
+        }
+ public void getDescription(){
+     System.out.println("Fresh chocolate chip")
+ }
+  public static void main(String args[]){
+	Cookie obj = new Cookie();
+	obj.getDescription();
+ 
+}
+  public void getSellByDate(){
+      System.out.println("11/11/20")
+  }
+   public static void main(String args[]){
+	Cooke obj = new Cookie();
+	obj.getSellByDate();
+}
+}
