ea096bba85591a785569a0062c2499354eaac9a4
shak rajbhandari <shak@shaks-MacBook-Pro-2.local>
2020-04-13 03:48:16 -0400

    new commit

---
+++ Brownie.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comp730finals;
+
+/**
+ *
+ * @author shak
+ */
+public class Brownie implements BakedGoods{
+            public void getPrice(){
+    
+
+        System.out.println("2")
+}
+ public static void main(String args[]){
+	BakedGoods obj = new Brownie();
+	obj.getPrice();
+    
+
+        }
+ public void getDescription(){
+     System.out.println("walnut filled")
+ }
+  public static void main(String args[]){
+	Cookie obj = new Browniel();
+	obj.getDescription();
+ 
+}
+  public void getSellByDate(){
+      System.out.println("10/15/20")
+  }
+   public static void main(String args[]){
+	Cooke obj = new Brownie();
+	obj.getSellByDate();
+}
+}
+    
+
