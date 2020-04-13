d370d7c8932ca3c4a4f6c87b07723506c4879f6d
shak rajbhandari <shak@shaks-MacBook-Pro-2.local>
2020-04-13 03:49:04 -0400

    new commit

---
+++ CinnamonRoll.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comp730finals;
+
+/**
+ *
+ * @author shak
+ */
+public class CinnamonRoll implements BakedGoods{
+        public void getPrice(){
+    
+
+        System.out.println("3")
+}
+ public static void main(String args[]){
+	BakedGoods obj = new  CinnamonRoll();
+	obj.getPrice();
+    
+
+        }
+ public void getDescription(){
+     System.out.println("new square cinnamon roll")
+ }
+  public static void main(String args[]){
+	Cookie obj = new  CinnamonRoll();
+	obj.getDescription();
+ 
+}
+  public void getSellByDate(){
+      System.out.println("09/05/20")
+  }
+   public static void main(String args[]){
+	Cooke obj = new  CinnamonRoll();
+	obj.getSellByDate();
+}
+}
